d59f0dbd1049d2776a1877fab53dbceb545ed3ba
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-12-19 11:32:44 +0900

    パーティションを /ext-hdd/data/ にマウント

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -11,11 +11,18 @@ sudo  parted  --script --align optimal  /dev/sdc -- mklabel gpt
 sudo  parted  --script --align optimal  /dev/sdc -- mkpart primary ext3 1 -1
 sudo  mkfs.ext3    /dev/sdc1
 
+sudo  mkdir  -p    /ext-hdd/data
+sudo  chmod  1777  /ext-hdd/data
+
+echo  -e  "/dev/sdc1\t/ext-hdd/data\text3\tdefaults\t0\t0"  \
+    |  sudo  tee -a  /etc/fstab
+
 # RamDisk
 sudo  mkdir        /ramdisk
 sudo  chmod  1777  /ramdisk
 echo  -e  "tmpfs\t/ramdisk\ttmpfs\trw,size=2048m,x-gvfs-show\t0\t0"  \
-      |  sudo  tee -a  /etc/fstab
+    |  sudo  tee -a  /etc/fstab
+
 sudo  mount  -a
 
 date  >  /root/.provision.root
