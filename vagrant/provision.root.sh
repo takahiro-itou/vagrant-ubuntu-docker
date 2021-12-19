1718617b10a0fcc0a019a24b6e7f65c0316612ac
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-12-19 11:28:50 +0900

    新しく接続したハードディスクにパーティションを作成

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -6,6 +6,10 @@ echo  Provisioning $HOSTNAME
 
 sudo  timedatectl  set-timezone Asia/Tokyo
 
+# New HDD (/dev/sdc)
+sudo  parted  --script --align optimal  /dev/sdc -- mklabel gpt
+sudo  parted  --script --align optimal  /dev/sdc -- mkpart primary ext3 1 -1
+
 # RamDisk
 sudo  mkdir        /ramdisk
 sudo  chmod  1777  /ramdisk
