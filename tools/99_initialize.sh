201bc2835b60340a5d36db65734e5a685256b6a8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-23 19:35:23 +0900

    サブリポジトリの更新に合わせて修正

--- tools/99_initialize.sh
+++ tools/99_initialize.sh
@@ -6,8 +6,11 @@ vagrant_dir="${script_dir}/../vagrant"
 # 所定のディレクトリを、仮想マシンへ転送する用意をする
 project_base_dir=$(readlink -f "${script_dir}/..")
 
-env WORKDIR='/cygdrive/w/Vagrant'   \
-    /bin/bash -xue "${vagrant_dir}/setup-userdata.sh"   \
+env     \
+    COPY_CONFIG_DIR='yes'                       \
+    PROJECT_BASE_DIR="${project_base_dir}"      \
+    WORKDIR='/cygdrive/w/Vagrant'               \
+    /bin/bash -xue "${vagrant_dir}/common/setup-userdata.sh"    \
     || exit $?
 
 pushd "${vagrant_dir}"
